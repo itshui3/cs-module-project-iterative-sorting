7e6de24c6841b296ac31ef38c43692298831e588
Hui <jeffrey.x.hui@gmail.com>
2020-06-29 18:09:36 -0700

    insertion_sort

---
+++ src/iterative_sorting/insertion_sort.py
@@ -0,0 +1,24 @@
+
+
+def insertion_sort(arr):
+    # i marks the beginning of the unsorted sub-list
+    for i in range(1, len(arr)):
+
+        curdex = i
+        while curdex > 0:
+
+            if arr[curdex] >= arr[curdex - 1]:
+                break
+            else:
+                # arr[curdex - 1], arr[curdex] = arr[curdex], arr[curdex - 1]
+                right_val = arr[curdex]
+                left_val = arr[curdex - 1]
+                arr[curdex] = left_val
+                arr[curdex - 1] = right_val
+
+                curdex -= 1
+    return arr
+
+unsorted = [0, 5, 2, 3, 1, 8, 9, 2, 1, 3]
+
+print(insertion_sort(unsorted))
